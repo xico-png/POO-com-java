3700bd39083b64fb90508ea491210fe9f351c9b8
xico-png <luizfranxico1010@gmail.com>
2026-02-10 14:27:19 -0300

    feat: hello world pro cris!

---
+++ Aula1/aula_1.java
@@ -0,0 +1,6 @@
+public class aula_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Um Abraço pro professor Cris!!");
+    }
+}
