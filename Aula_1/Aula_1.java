a20a72954eeab6f41df6d152c16fac34233210d1
xico-png <luizfranxico1010@gmail.com>
2026-02-24 13:38:53 -0300

    exercicios de exemplo do cris

--- Aula_1/Aula_1.java
+++ Aula_1/Aula_1.java
@@ -1,6 +1,7 @@
-public class aula_1 {
+public class Aula_1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Um Abraço pro professor Cris!!");
     }
 }
+
---
+++ Aula_2.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Aula_2{
+    public static void main(String[] args) {
+        double NPA;
+
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Digite a NPA:");
+            NPA = scanner.nextDouble();
+            if(NPA > 50.0) System.out.println("Aprovado");
+            else System.out.println("Voce esta NP3");
+
+           System.out.println("Digite a sua nota da np3"); 
+              double NP3 = scanner.nextDouble();
+              if((NP3 + NPA)/2 > 50.0) System.out.println("Aprovado");
+              else System.out.println("Reprovado");
+           
+
+        
+        }
+
+    
+    }
+ 
+
+}
+
